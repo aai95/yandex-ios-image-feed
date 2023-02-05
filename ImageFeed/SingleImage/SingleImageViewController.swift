90460cda09aa57e0ed368c83c0183eb87f7972d6
Aleksei Izmalkin <aai95@yandex.ru>
2023-02-05 20:26:27 +0400

    Configure Segue to show selected photo on SingleImage view

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -2,5 +2,17 @@ import UIKit
 
 final class SingleImageViewController: UIViewController {
     
-    @IBOutlet var imageView: UIImageView!
+    @IBOutlet private var imageView: UIImageView!
+    
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
 }
