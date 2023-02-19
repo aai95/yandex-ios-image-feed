b85ea3949775f6687924e59dfe27c16c7220c1ad
Aleksei Izmalkin <aai95@yandex.ru>
2023-02-19 20:19:18 +0400

    Add constants to use Unsplash

---
+++ ImageFeed/Helpers/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let AccessKey = "K33Q2u80wENwU-qFQHkeLiSNTpeuVBPzVUqFQCLcwgY"
+let SecretKey = "cY85sH253OxfF_T6wCwu2isIZqk_liym5fpTrkLaMvA"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
