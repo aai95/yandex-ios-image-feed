9319aff372adf360113178263bc88be90f2cc0c5
Aleksei Izmalkin <aai95@yandex.ru>
2023-02-22 19:39:07 +0400

    Prepare Auth and Web ViewControllers with Navigation between them

---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+import WebKit
+
+final class WebViewViewController: UIViewController {
+    
+    @IBOutlet private var webView: WKWebView!
+    
+    @IBAction private func didTapBackButton() {}
+}
