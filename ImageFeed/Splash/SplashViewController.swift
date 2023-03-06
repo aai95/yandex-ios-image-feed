466ba9cb6d433928f3db887585ac05a61d98c424
Aleksei Izmalkin <aai95@yandex.ru>
2023-03-06 15:01:14 +0400

    Import ProgressHUD dependency to project

--- ImageFeed/Splash/SplashViewController.swift
+++ ImageFeed/Splash/SplashViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import ProgressHUD
 
 final class SplashViewController: UIViewController {
     
@@ -50,6 +51,7 @@ final class SplashViewController: UIViewController {
 extension SplashViewController: AuthViewControllerDelegate {
     
     func authViewController(_ vc: AuthViewController, didAuthenticateWithCode code: String) {
+        ProgressHUD.show()
         dismiss(animated: true) { [weak self] in
             guard let self = self else {
                 return
@@ -66,8 +68,9 @@ extension SplashViewController: AuthViewControllerDelegate {
             switch result {
             case .success:
                 self.switchToTabBarController()
+                ProgressHUD.dismiss()
             case .failure:
-                break
+                ProgressHUD.dismiss()
             }
         }
     }
